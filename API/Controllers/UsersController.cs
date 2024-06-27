a499eb446468877b67f3287c25a00cae7d4840d8
Kaung Htet Win <kaunghtetwynn@gmail.com>
2024-06-28 02:13:31 +0800

    feat: add api structure (s2)

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController: ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UsersController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+
+        // await the part of the code that has the potential to block our requests
+        var users = await _context.Users.ToListAsync();
+
+        // returning http response
+        // we can return different type of http response because of ActionResult
+        //return Ok(users);
+        return users;
+        
+    }
+
+    //[HttpGet("{id}")]
+    [HttpGet("{id:int}")] // type safety
+    public async Task<ActionResult<AppUser>> GetUser(int id) {
+
+        var user = await _context.Users.FindAsync(id);
+        
+        if(user != null)
+            return user;
+
+        return NotFound();
+    }
+}
---
+++ API/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+public class AppUser
+{
+    // Default Id Column in EF Core is Id
+    // If you want UserId then you need to decorate it with [Key] attribute
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
